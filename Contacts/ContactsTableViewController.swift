21953700e5807a68e560ba5cc047126da871f21b
Aanya Jhaveri <Aanya@aanyas-laptop.local>
2017-07-27 21:47:45 -0500

    fixed cell bug

--- Contacts/ContactsTableViewController.swift
+++ Contacts/ContactsTableViewController.swift
@@ -39,7 +39,7 @@ class ContactsTableViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
         cell.textLabel?.text = "A fine example of a UITableViewCell"
 
